0ce6cb912bbd7ceabc267a59bed3173a0d64a1f7
Jess Telford <jess+github@jes.st>
2015-08-27 07:31:57 +1000

    AABB Collision mixin

---
+++ src/mixins/collidable.js
@@ -0,0 +1,89 @@
+'use strict';
+
+function calculateAABB(bounds) {
+
+  // Calculate Axis Aligned Bounding Box (AABB)
+  var min = {
+      x: Infinity,
+      y: Infinity
+    },
+    max = {
+      x: -Infinity,
+      y: -Infinity
+    };
+
+  bounds.forEach(function(point) {
+    min.x = Math.min(point.x, min.x);
+    min.y = Math.min(point.y, min.y);
+    max.x = Math.max(point.x, max.x);
+    max.y = Math.max(point.y, max.y);
+  });
+
+  return {
+    x: min.x,
+    y: min.y,
+    w: max.x - min.x,
+    h: max.y - min.y
+  }
+}
+
+module.exports = {
+
+  isCollidable: true,
+
+  /**
+   * @param bounds Array a list of points ({x, y}) in which make up the bounding
+   * area in clockwise / left-hand winding order
+   */
+  setCollisionBounds: function(bounds) {
+    this.bounds = bounds;
+    this._calcBounds = calculateAABB(this.bounds);
+  },
+
+  getAABB: function() {
+
+    var aabb = {
+      x: this._calcBounds.x,
+      y: this._calcBounds.y,
+      w: this._calcBounds.w,
+      h: this._calcBounds.h
+    };
+
+    if (this.isScalable) {
+      aabb.x *= this.scale;
+      aabb.y *= this.scale;
+      aabb.w *= this.scale;
+      aabb.h *= this.scale;
+    }
+
+    // When it's movable, translate the collision bounds
+    if (this.isMovable) {
+      aabb.x += this.x;
+      aabb.y += this.y;
+    }
+
+    return aabb;
+
+  },
+
+  // TODO: Better than AABB collision
+  collidingWith: function(collidable) {
+
+    var bounds = this.getAABB();
+    var otherBounds = collidable.getAABB();
+
+    // Do AABB collision
+    if (
+      bounds.x < otherBounds.x + otherBounds.w
+      && bounds.x + bounds.w > otherBounds.x
+      && bounds.y < otherBounds.y + otherBounds.h
+      && bounds.h + bounds.y > otherBounds.y
+    ) {
+      return true;
+    }
+
+    return false;
+
+  }
+
+};
--- src/player.js
+++ src/player.js
@@ -4,6 +4,7 @@ var movable = require('./mixins/movable'),
     scalable = require('./mixins/scalable'),
     strokable = require('./mixins/strokable'),
     colourable = require('./mixins/colourable'),
+    collidable = require('./mixins/collidable'),
     renderable = require('./mixins/renderable'),
     objectAssign = require('object-assign');
 
@@ -11,15 +12,30 @@ var path = new Path2D('M-10.5 -4.5 l20 4 l-20 4 l5 -4 l-5 -4');
 
 module.exports = function getPlayer(ctx) {
 
-  return objectAssign({}, movable, colourable, scalable, strokable, renderable, {
+  var player = objectAssign(
+    {},
+    movable,
+    colourable,
+    collidable,
+    scalable,
+    strokable,
+    renderable,
+    {
+      x: 50,
+      y: 50,
+      colour: 'blue',
+      scale: 5,
+      lineWidth: 1,
+      path: path,
+      ctx: ctx
+    }
+  );
 
-    x: 50,
-    y: 50,
-    colour: 'blue',
-    scale: 1,
-    lineWidth: 1,
-    path: path,
-    ctx: ctx
+  player.setCollisionBounds([
+    {x: -10.5, y: -4.5},
+    {x: 10.5, y: 0.5},
+    {x: -10.5, y: 4.5}
+  ]);
 
-  });
+  return player;
 };
