f5103d5d485ea70da5f36e7d22c60679ac792d1b
Jess Telford <jess+github@jes.st>
2015-08-29 17:36:00 +1000

    Document cache usage

--- src/js/cache-generator.js
+++ src/js/cache-generator.js
@@ -2,6 +2,16 @@
 
 var caches = new Map();
 
+/**
+ * Create caches to store arbitrary sets of data.
+ *
+ * Data stored in the created caches will be deduplicated.
+ * Caches are global based on the cacheName
+ *
+ * @param cacheName String A UUID to identify this cache
+ *
+ * @return Object The cache.
+ */
 module.exports = function(cacheName) {
 
   var newCache,
@@ -15,6 +25,14 @@ module.exports = function(cacheName) {
 
   newCache = {
 
+    /**
+     * @param constructor Function When creating a new item, use this
+     * @param selector Function called with single argument: (cache). Return
+     * either an element from the cache which should be used, or null to
+     * generate a new one
+     *
+     * @return Mixed the selected or created cache item
+     */
     get: function(constructor, selector) {
 
       var result;
@@ -49,12 +67,18 @@ module.exports = function(cacheName) {
     },
   };
 
-  // Allow iteration with for...of
+  /**
+   * Allow iteration of the cache values using ES6 Iterators
+   *
+   * @return Iterable
+   */
   newCache[Symbol.iterator] = function() {
     return cache[Symbol.iterator]();
   }
 
+  // Store this newly created cache in a global cache of caches... CACHE!
   caches.set(cacheName, newCache);
+
   return newCache;
 
 }
