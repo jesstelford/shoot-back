232b164f92baf3aba33b66875e953971a71f20a3
Jess Telford <jess+github@jes.st>
2015-09-03 22:03:43 +1000

    Generic circle movement

--- src/js/keyframes/circle.js
+++ src/js/keyframes/circle.js
@@ -10,13 +10,19 @@ module.exports = function(radius, angleChangePerFrame) {
 
     state.radius = state.radius || radius;
 
-    // initial position is at the bottom of the circle
-    state.angle = state.angle || Math.PI / 2;
+    // initial position is at the top or bottom of the circle
+    state.angle = state.angle || (angleChangePerFrame > 0 ? Math.PI / 2 : -Math.PI / 2);
     state.lastX = state.lastX || 0;
-    state.lastY = state.lastY || state.radius;
+    state.lastY = state.lastY || (angleChangePerFrame > 0 ? state.radius : -state.radius);
 
     // rotate clockwise
     state.angle += step * angleChangePerFrame;
+
+    if (state.angle > Math.PI * 2) {
+      state.angle -= Math.PI * 2;
+    } else if (state.angle < 0) {
+      state.angle += Math.PI * 2;
+    }
 
     newX = state.radius * Math.cos(state.angle);
     newY = state.radius * Math.sin(state.angle);
