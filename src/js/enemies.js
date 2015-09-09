7764782cb7ff9e1618362666b023f50c02126487
Jess Telford <jess+github@jes.st>
2015-09-10 08:11:11 +1000

    Sine wave keyframe movement

--- src/js/enemies.js
+++ src/js/enemies.js
@@ -4,8 +4,67 @@ var getEnemy = require('./enemy'),
     random = require('./random'),
     cache = require('./cache-generator')('enemies'),
     forOf = require('./utils/for-of'),
+    keyframeSin = require('./keyframes/sin'),
     keyframeCircle = require('./keyframes/circle'),
     objectAssign = require('object-assign');
+
+function fullWave(when, duration, waves, amplitude, direction) {
+
+  var radiansInCircle = Math.PI * 2,
+      rotationRate = direction * waves * radiansInCircle / (60 * duration / 1000);
+
+  return {
+    when: when,
+    func: 'move',
+    params: keyframeSin(amplitude, rotationRate, 'middle', 'y'),
+    loopFor: duration
+  }
+}
+
+function circle(when, duration, radius, direction) {
+
+  var radiansInCircle = Math.PI * 2,
+      rotationRate = direction * radiansInCircle / (60 * duration / 1000);
+
+  return {
+    when: when,
+    func: 'move',
+    params: keyframeCircle(radius, rotationRate),
+    loopFor: duration
+  }
+}
+
+function semiCircle(when, duration, radius, direction) {
+
+  var radiansInCircle = Math.PI * 2,
+      rotationRate = direction * radiansInCircle / (60 * duration / 1000) / 2;
+
+  return {
+    when: when,
+    func: 'move',
+    params: keyframeCircle(radius, rotationRate),
+    loopFor: duration
+  }
+}
+
+function figure8(when, durationPerLoop, radius) {
+  return [
+    circle(when, durationPerLoop, radius, -1),
+    circle(when + durationPerLoop, durationPerLoop, radius, 1)
+  ]
+}
+
+function move(when, duration, rate) {
+  return {
+    when: when,
+    func: 'move',
+    params: function(elapsedTime) {
+      var step = elapsedTime / (1000 / 60);
+      return [step * rate, 0];
+    },
+    loopFor: duration
+  }
+}
 
 var types = [
   {
@@ -17,37 +76,29 @@ var types = [
     ],
     rotation: Math.PI,
     keyframes: [
-      {
-        when: 0,
-        func: 'move',
-        params: function(elapsedTime) {
-          var step = elapsedTime / (1000 / 60);
-          return [step * -4, 0];
-        },
-        loopFor: 2000
-      },
-      {
-        when: 2000,
-        func: 'move',
-        params: keyframeCircle(80, -0.05),
-        loopFor: 2000
-      },
-      {
-        when: 4000,
-        func: 'move',
-        params: keyframeCircle(80, 0.05),
-        loopFor: 2000
-      },
-      {
-        when: 6000,
-        func: 'move',
-        params: function(elapsedTime) {
-          var step = elapsedTime / (1000 / 60);
-          return [step * -4, 0];
-        },
-        loopFor: -1
-      },
-    ]
+      move(0, 1000, -4),
+      move(1000, 5000, 1),
+      move(5000, -1, -4)
+    ].concat(
+      fullWave(1000, 4000, 2, 100, -1)
+      /* semiCircle(2000, 1000, 120, -1) */
+      /* figure8(2000, 4000, 120) */
+    )
+  },
+  {
+    path: new Path2D('M-10.5 -4.5 l20 4 l-20 4 l5 -4 l-5 -4'),
+    collision: [
+      {x: -10.5, y: -4.5},
+      {x: 10.5, y: 0.5},
+      {x: -10.5, y: 4.5}
+    ],
+    rotation: Math.PI,
+    keyframes: [
+      move(0, 2000, -4),
+      move(3000, -1, -4)
+    ].concat(
+      figure8(2000, 4000, 120)
+    )
   }
 ];
 
---
+++ src/js/keyframes/sin.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/**
+ * @param Integer amplitude The amplitude in px of the sine wave
+ * @param Float angleChangePerFrame How many radians to change per 16.6666ms
+ *                                  (ie; 60fps)
+ * @param Integer startAt 'bottom' for the bottom of the sine wave,
+ *                        'middle' for the middle,
+ *                        'top' for the top
+ * @param String axis 'x' or 'y'
+ */
+module.exports = function(amplitude, angleChangePerFrame, startAt, axis) {
+
+  return function sine(elapsedTime, state) {
+
+    var step = elapsedTime / (1000 / 60),
+        result,
+        newX,
+        newY;
+
+    state.amplitude = state.amplitude || amplitude;
+
+    if (state.angle === undefined) {
+      // initial position is at the top or bottom of the sine wave
+      if (startAt === 'bottom') {
+        state.angle = -Math.PI / 2;
+      } else if (startAt === 'top') {
+        state.angle = Math.PI / 2;
+      } else {
+        state.angle = 0;
+      }
+
+    }
+
+    if (state.angleChangePerFrame === undefined) {
+      state.angleChangePerFrame = angleChangePerFrame;
+    }
+
+    if (state.lastX === undefined) {
+
+      if (axis === 'x') {
+
+        let startVal = 0;
+
+        if (startAt === 'bottom') {
+          startVal = -state.amplitude;
+        } else if (startAt === 'top') {
+          startVal = state.amplitude;
+        }
+
+        state.lastX = startVal;
+
+      } else {
+
+        state.lastX = 0;
+      }
+    }
+
+    if (state.lastY === undefined) {
+
+      if (axis === 'y') {
+
+        let startVal = 0;
+
+        if (startAt === 'bottom') {
+          startVal = -state.amplitude;
+        } else if (startAt === 'top') {
+          startVal = state.amplitude;
+        }
+
+        state.lastY = startVal;
+
+      } else {
+
+        state.lastY = 0;
+      }
+
+    }
+
+    // rotate clockwise
+    state.angle += step * state.angleChangePerFrame;
+
+    if (state.angle > Math.PI / 2) {
+
+      // change directions
+      state.angleChangePerFrame *= -1;
+
+      // cap it at the end
+      state.angle = Math.PI / 2;
+
+    } else if (state.angle < -Math.PI / 2) {
+
+      // change directions
+      state.angleChangePerFrame *= -1;
+
+      // cap it at the end
+      state.angle = -Math.PI / 2;
+    }
+
+    if (axis === 'x') {
+      newX = state.amplitude * Math.sin(state.angle);
+      newY = 0;
+    } else {
+      newX = 0;
+      newY = state.amplitude * Math.sin(state.angle);
+    }
+
+    result = [newX - state.lastX, newY - state.lastY];
+
+    state.lastX = newX;
+    state.lastY = newY;
+
+    return result;
+  }
+}
